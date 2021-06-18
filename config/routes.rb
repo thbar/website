0b0fcdb60e179f54db1f85236f426fc692a34e9f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-18 11:18:12 +0200

    Working on maintaining teams

--- config/routes.rb
+++ config/routes.rb
@@ -259,6 +259,7 @@ Rails.application.routes.draw do
     resources :exercise_representations
     resources :tracks, only: [:show]
     resources :site_updates, except: [:destroy]
+    resources :teams, except: [:destroy]
   end
 
   namespace :contributing do
